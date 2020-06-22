8a0b96ca1ad6c3fdfb1f95f8afb02ccdb0e48a27
wei.zhang <37876877+Zhang-Wei-666@users.noreply.github.com>
2020-06-22 13:17:12 +0800

    提取类库文件到静态资源文件夹内

--- hu.config.js
+++ hu.config.js
@@ -1,6 +1,20 @@
+const { dirname, resolve } = require('path');
+const { copySync } = require('fs-extra');
 const PluginVue = require('rollup-plugin-vue');
 
 const HU_RUNNING_COMMAND = process.env.HU_RUNNING_COMMAND;
+
+
+// 拷贝 Vue 到静态资源文件夹
+{
+  const vueDirname = dirname(require.resolve('vue'));
+  const vuePath = resolve(vueDirname, 'vue.min.js');
+
+  copySync(
+    vuePath,
+    resolve(__dirname, 'client/static/lib/vue.min.js')
+  );
+}
 
 
 module.exports = {
@@ -10,19 +24,19 @@ module.exports = {
   inputDir: '',
   outputDir: '',
 
-  browserslist: [
-    'last 2 Chrome versions'
-  ],
-
-  plugins: () => [
-    PluginVue()
-  ],
+  banner: '/* eslint-disable */\n\n',
 
   replace: {
     'process.env.NODE_ENV': JSON.stringify(HU_RUNNING_COMMAND === 'build' ? 'production' : 'development')
   },
 
-  banner: '/* eslint-disable */\n\n',
+  externals: {
+    vue: 'Vue'
+  },
+
+  plugins: () => [
+    PluginVue()
+  ],
 
   pluginOptions: {
     banner: {
@@ -30,10 +44,14 @@ module.exports = {
     }
   },
 
+  browserslist: [
+    'last 2 Chrome versions'
+  ],
+
   pipe: [
     {
       input: 'src/index.js',
-      output: 'client/static/index.js'
+      output: 'client/index/index.js'
     }
   ]
 };
